a931537be21c29a4528852d15d3d7aa7f41e96e0
venilson2 <venilson2009@hotmail.com>
2024-04-29 21:53:21 -0300

    feat: route default controller Participante and Action Create

--- Program.cs
+++ Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Participante}/{action=Create}/{id?}");
 
 app.Run();
